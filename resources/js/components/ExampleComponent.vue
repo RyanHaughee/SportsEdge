d36387b87d240b3d9deccb83655d30cecb71983d
Ryan Haughee <ryanhaughee@gmail.com>
2024-01-02 21:54:47 -0600

    created laravel scaffold and four models

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Hello, Vue in Laravel!</h1>
+    </div>
+</template>
+
+<script>
+// Ensure you export the component as ExampleComponent
+export default {
+    name: 'ExampleComponent',
+    // other component properties...
+}
+</script>
+
+<style>
+    /* Your component styles */
+</style>
